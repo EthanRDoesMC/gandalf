7b8a1b2ef173b0be6c407d232bcd4bceae3fc90a
Wolf Posdorfer <posdorfer@informatik.uni-hamburg.de>
2017-02-03 16:43:46 +0100

    adding ifile, optimizing script

--- create.sh
+++ create.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Settings
+basename="Gandalf10"
+conflictfile="conflicts.txt"
 
-echo "Setting up Files"
 # create control file and setup for conflicts
-controlfile="Gandalf10/DEBIAN/control"
-mkdir -p "Gandalf10/DEBIAN/"
-cp control Gandalf10/DEBIAN/control
+echo "Setting up Files"
+controlfile="$basename/DEBIAN/control"
+mkdir -p "$basename/DEBIAN/"
+cp control $controlfile
 
 
 echo -e "Conflicts: \c" >> $controlfile
@@ -14,24 +17,23 @@ echo -e "Conflicts: \c" >> $controlfile
 # parse conflicts
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-	dt="$(echo "$line"|tr -d '\n')"
-	echo "$dt"
+	dt="$(echo "$line"|tr -d '\r\n')"
     echo -e "$dt\c" >> $controlfile 
-done < "conflicts.txt"
+done < "$conflictfile"
 echo "" >> $controlfile
 
 
 # make dummy file
-mkdir -p Gandalf10/var/mobile/Downloads/
-echo "This is a dummy file from Gandalf iOS 10" >> Gandalf10/var/mobile/Downloads/dummy.txt
+mkdir -p $basename/var/mobile/Downloads/
+echo "This is a dummy file from Gandalf iOS 10" >> $basename/var/mobile/Downloads/gandalf_10_dummy.txt
 
 
 
 # package
 echo "Packaging"
-dpkg-deb -Zgzip -b Gandalf10
+dpkg-deb -Zgzip -b $basename
 
 
 # cleanup
 echo "Cleaning up leftovers"
-rm -r Gandalf10
+rm -r $basename
