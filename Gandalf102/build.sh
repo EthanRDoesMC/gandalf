47941f427a70dffe7e035689cc69968cbb286d44
mehulrao <mehulrao@Loft-Mac.local>
2017-04-20 17:04:25 -0400

    Added the scripts for now
    Not a release version

--- Gandalf102/build.sh
+++ Gandalf102/build.sh
@@ -23,12 +23,20 @@ while IFS='' read -r line || [[ "$line" ]]; do
     echo "$dt" >> $controlfile 
 done < "$conflictfile"
 echo -n "" >> $controlfile
+echo "Making The Tar"
 tar -czf gandalf.app.tar.gz gandalf.app
 mkdir -p $basename/Applications
 mkdir -p $basename/var/mobile/Downloads/Gandalf102
 mv gandalf.app.tar.gz $basename/var/mobile/Downloads/Gandalf102
 cp prerm $basename/DEBIAN
 cp postinst $basename/DEBIAN
+
+
+# Do the scripts
+echo "Doing The Scripts"
+mkdir -p $basename/usr/bin
+cp gandalf102-hold $basename/usr/bin
+cp gandalf102-rmhold $basename/usr/bin
 
 # make managerlist file - make sure to update based on version
 echo "Gandalf for Yalu102 (2.0 Delta 3) was installed via Manager." >> $basename/var/mobile/Downloads/Gandalf102/io.github.ethanrdoesmc.gandalf102.txt
@@ -42,3 +50,7 @@ dpkg-deb -Zgzip -b $basename
 # cleanup
 echo "Removing temporary folders and files."
 rm -r $basename
+
+sleep 1
+
+echo "We made it"
