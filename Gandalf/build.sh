e5cfe99ce09d4da176b644c3d6c506776a3a1a26
jonisc <you@example.com>
2017-05-03 15:56:28 +0200

    improved postinst and build.sh has more output

--- Gandalf/build.sh
+++ Gandalf/build.sh
@@ -3,18 +3,19 @@
 #Please run with a Linux flavor. macOS makes a lot of dummy files.
 
 # Aliases
+
 basename="io.github.ethanrdoesmc.gandalf102" #IDENTIFIER
-conflictfile="conflicts102.txt"
+conflictfile="conflicts102.txt" # Name of conflictsfile
 scriptname="gandalf102" #NAME OF THE SCRIPT
 productname="Gandalf 102" #NAME OF GANDALF SHOWN IN TERMINAL
 
 # create control file and setup for conflicts
-echo "Creating directories and copying files."
+echo "Creating directories and copying files..."
 controlfile="$basename/DEBIAN/control"
 mkdir -p "$basename/DEBIAN/"
 cp control102 $controlfile
 
-#CODE CLEANUP NEEDS TO HAPPEN.
+#CODE CLEANUP NEEDS TO HAPPEN. -> Define all variables at beginning?
 #We aren't adhering to standards very well.
 
 
@@ -26,19 +27,27 @@ echo "Breaks: ${conflicts}" >> $controlfile
 
 echo "Making the TAR..."
 tar -czf Gandalf.app.tar.gz gandalf.app
+echo "Creating folders..."
 mkdir -p $basename/Applications
 mkdir -p $basename/var/mobile/Downloads/Gandalf102
+echo "Moving TAR around..."
 mv Gandalf.app.tar.gz $basename/var/mobile/Downloads/Gandalf102
+echo "Copying remove and install scripts..."
 cp prerm $basename/DEBIAN
 cp postinst $basename/DEBIAN
+# edit postinst
+echo "Adding right hold command to install script..."
+sed -i "s/gAndAlfh0LdC0mMand/$scriptname/" $basename/DEBIAN/postinst
 
 
 # Do the scripts
 ###
 
 echo "Scripted Scripting is Scriptatious and Scriptmatic"
+echo "or simply: Messing around with the hold script..."
 mkdir -p $basename/usr/bin
 cp $scriptname $basename/usr/bin
+echo "Editing hold script..."
 sed -i "s/iDenT1FIEr/$basename/" $basename/usr/bin/$scriptname
 sed -i "s/vErs10Nname/$productname/" $basename/usr/bin/$scriptname
 ### might work with sed (works on UBUNTU)
@@ -49,12 +58,13 @@ sed -i "s/vErs10Nname/$productname/" $basename/usr/bin/$scriptname
 
 
 # package
-echo "Creating package."
+echo "Almost finished..."
+echo "Creating package..."
 dpkg-deb -Zgzip -b $basename
 
 
 # cleanup
-echo "Removing temporary folders and files."
+echo "Removing temporary folders and files..."
 rm -r $basename
 
 sleep 1
