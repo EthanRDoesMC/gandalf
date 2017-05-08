35bd766c88a1b8cb68a5851ddc4ab6321dc7c22e
EthanRDoesMC <ethanrdoesmc@gmail.com>
2017-05-08 12:02:24 -0400

    2.5, app is working, unified buildscript

--- Gandalf/build.sh
+++ Gandalf/build.sh
@@ -4,23 +4,28 @@
 #
 # Follow https://google.github.io/styleguide/shell.xml
 # for the most part (You can ignore some, like error checking for mv)
+echo "We're trying something new! Put in the version you want in version.txt (either as 'x' or 102) and we'll have one build.sh script!"
+echo "sleeping for 5 seconds"
+sleep 5
+VER=$(cat version.txt)
 
 # Config
-PKG_VERSION="2.4.8" #Bump this everytime you update something.
-CONFLICTS_FILE="conflicts102.txt"
-
+PKG_VERSION="2.5" #Bump this everytime you update something.
+CONFLICTS_FILE="$VER/conflicts.txt"
+NAME=$(cat $VER/name.txt)
+FIRM=$(cat $VER/firmware.txt)
 #DO NOT TOUCH! (Unless you have a good reason...)
 #Variable format is "PKG_FIELDNAME"
-PKG_PACKAGE="io.github.ethanrdoesmc.gandalf102"
-PKG_NAME="Gandalf for Yalu102"
+PKG_PACKAGE="io.github.ethanrdoesmc.gandalf$VER"
+PKG_NAME="Gandalf for $NAME"
 PKG_DESCRIPTION="Some tweaks may break jailbreaks. Let this tweak say
   \"You Shall Not Pass!\" to incompatible tweaks and you can sit back and have
   fun with your jailbreak."
 PKG_DEPICTION="https://ethanrdoesmc.github.io/gandalf/depictions/?p=io.github.ethanrdoesmc.gandalf102"
 PKG_MAINTAINER="EthanRDoesMC <ethanrdoesmc@gmail.com>"
 PKG_AUTHOR="EthanRDoesMC <ethanrdoesmc@gmail.com>"
-PKG_SECTION="Gandalf102"
-PKG_DEPENDS="firmware (>=10.0), sudo, com.officialscheduler.mterminal, mobilesubstrate"
+PKG_SECTION=$(cat $VER/section.txt)
+PKG_DEPENDS="firmware $FIRM, sudo, com.officialscheduler.mterminal, mobilesubstrate"
 PKG_REPLACES="com.enduniverse.cydiaextenderplus, com.github.ethanrdoesmc.gandalf, com.github.ethanrdoesmc.gandalf102"
 PKG_ARCHITECTURE='iphoneos-arm'
 PKG_BREAKS=$(cat ${CONFLICTS_FILE} | sed ':a;N;$!ba;s/\n/,\ /g')
@@ -37,7 +42,7 @@ echo "Creating package structure..."
 mkdir "${PKG_PACKAGE}"
 mkdir "${PKG_PACKAGE}/DEBIAN"
 mkdir -p "${PKG_PACKAGE}/usr/bin"
-mkdir -p "${PKG_PACKAGE}/var/mobile/Downloads/Gandalf102"
+mkdir -p "${PKG_PACKAGE}/var/mobile/Downloads/Gandalf"
 
 
 #Create the control file
@@ -63,7 +68,7 @@ EOF
 echo "Compressing and moving Gandalf.app..."
 
 tar -czf "Gandalf.app.tar.gz" "Gandalf.app"
-mv "Gandalf.app.tar.gz" "${PKG_PACKAGE}/var/mobile/Downloads/Gandalf102"
+mv "Gandalf.app.tar.gz" "${PKG_PACKAGE}/var/mobile/Downloads/Gandalf"
 
 
 #Copy over the executable
