c30e5314d168d88d66beeb9d64cdc4dc71e11848
1Conan <1Conan@users.noreply.github.com>
2017-05-10 20:14:19 +0900

    Fix some more styling stuff

--- Gandalf/build.sh
+++ Gandalf/build.sh
@@ -11,26 +11,29 @@
 PKG_VERSION="2.5.1" #Bump this everytime you update something.
 
 #DO NOT TOUCH! (Unless you have a good reason...)
+	
+#script specific variables
+VER=$(cat version.txt)
+CONFLICTS_FILE="${VER}/conflicts.txt"
+NAME=$(cat ${VER}/name.txt)
+FIRM=$(cat ${VER}/firmware.txt)
+	
 #Variable format is "PKG_FIELDNAME"
-PKG_PACKAGE="io.github.ethanrdoesmc.gandalf$VER"
-PKG_NAME="Gandalf for $NAME"
+PKG_PACKAGE="io.github.ethanrdoesmc.gandalf${VER}"
+PKG_NAME="Gandalf for ${NAME}"
 PKG_DESCRIPTION="Some tweaks may break jailbreaks. Let this tweak say
   \"You Shall Not Pass!\" to incompatible tweaks and you can sit back and have
   fun with your jailbreak."
 PKG_DEPICTION="https://ethanrdoesmc.github.io/gandalf/depictions/?p=io.github.ethanrdoesmc.gandalf102"
 PKG_MAINTAINER="EthanRDoesMC <ethanrdoesmc@gmail.com>"
 PKG_AUTHOR="EthanRDoesMC <ethanrdoesmc@gmail.com>"
-PKG_SECTION=$(cat $VER/section.txt)
-PKG_DEPENDS="firmware $FIRM, sudo, com.officialscheduler.mterminal, mobilesubstrate"
+PKG_SECTION=$(cat ${VER}/section.txt)
+PKG_DEPENDS="firmware ${FIRM}, sudo, com.officialscheduler.mterminal, mobilesubstrate"
 PKG_REPLACES="com.enduniverse.cydiaextenderplus, com.github.ethanrdoesmc.gandalf, com.github.ethanrdoesmc.gandalf102"
 PKG_ARCHITECTURE='iphoneos-arm'
 PKG_BREAKS=$(cat ${CONFLICTS_FILE} | sed ':a;N;$!ba;s/\n/,\ /g')
 
-#script specific variables
-VER=$(cat version.txt)
-CONFLICTS_FILE="${VER}/conflicts.txt"
-NAME=$(cat ${VER}/name.txt)
-FIRM=$(cat ${VER}/firmware.txt)
+
 
 
 #Main script
@@ -39,17 +42,17 @@ FIRM=$(cat ${VER}/firmware.txt)
 echo "We're trying something new! Put in the version you want in version.txt (either as 'x' or 102) and we'll have one build.sh script!"
 read -p "Continue? [Y/n]: " cont
 case $cont in 
-    [Yy]* )
-		continue
+  [Yy]* )
+    continue
     ;;
-    [Nn]* )
-		echo "Exiting..."
-		exit 0
-	;;
-	* ) 
-	    echo "Cancelled."
-		exit 0
-	;;
+  [Nn]* )
+    echo "Exiting..."
+    exit 0
+    ;;
+  * ) 
+    echo "Cancelled."
+    exit 0
+    ;;
 esac
 
 #Start message
