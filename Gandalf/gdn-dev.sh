c57bacb5c6a3a36af630ffe960c25f8a45812994
jonisc <jonisc@t-online.de>
2017-06-26 23:01:58 +0200

    First Styling update

--- Gandalf/gdn-dev.sh
+++ Gandalf/gdn-dev.sh
@@ -13,7 +13,9 @@ fi
 
 PACKAGE_REGEX="io\\.github\\.ethanrdoesmc\\.gandalf"
 # Functions
-function build_all {
+# build_all: Build all gandalf versions at once
+
+function build_all () {
  echo "Building all gandalf versions..."
  # Get number of lines of the file all_versions.txt and save them in a variable
  echo "We currently have $(wc -l < all_versions.txt) known Gandalf versions."
@@ -25,48 +27,49 @@ function build_all {
  # Get shortname of versions in all_versions.txt by deleting io.github.ethanrdoesmc.gandalf and compile gandalf as long as there is still a version --Comment not good, please edit--
 
  for VERSION in $(cat all_versions.txt | sed "s/^${PACKAGE_REGEX}//g"); do
- ./build.sh "${VERSION}"
- # Check if packaging was not successful; First save exitcode of build.sh in variable to make it possible to better work with it. 
-
-BUILDSH_ERRORCODE=$(echo $?)
-if [ "${BUILDSH_ERRORCODE}" -ne "0" ]; then
- echo "${RED}FATAL:${NORMAL} build.sh exited with error code '${BUILDSH_ERRORCODE}'. Build was most likely NOT successful."
- exit ${BUILDSH_ERRORCODE}
-fi
+  ./build.sh "${VERSION}"
+  # Check if packaging was not successful; First save exitcode of build.sh in variable to make it possible to better work with it. 
+  BUILDSH_ERRORCODE=$(echo $?)
+  if [ "${BUILDSH_ERRORCODE}" -ne "0" ]; then
+   echo "${RED}FATAL:${NORMAL} build.sh exited with error code '${BUILDSH_ERRORCODE}'. Build was most likely NOT successful."
+   exit ${BUILDSH_ERRORCODE}
+ fi
 done
 }
 case $1 in
 "compile-for-repo")
- build_all
- mv *.deb ../docs/
- sh ../docs/update.sh
+  build_all
+  mv *.deb ../docs/
+  sh ../docs/update.sh
 ;;
+
 "compile-all")
- build_all
+  build_all
 ;;
+
 "new")
- read -p "Please enter shortname (eg. 'c'):" FOLDER_NAME
- echo ${FOLDER_NAME}
- mkdir ${FOLDER_NAME}
- 
- read -p "Please enter the supported firmware (format: =10.2 or <8.4):" FILE_FIRMWARE
- printf "(${FILE_FIRMWARE})" > ${FOLDER_NAME}/firmware.txt
- echo "Gandalf is usually named after a jailbreak: Gandalf for <jailbreakname>. So: "
- read -p "Please enter the name of the jailbreak here:" FILE_NAME
- printf "${FILE_NAME}" > ${FOLDER_NAME}/name.txt
- read -p "Please enter the section now:" FILE_SECTION
- printf "${FILE_SECTION}" > ${FOLDER_NAME}/section.txt
- echo "Now there comes the most important step: you must now add all bundle identifiers to the file conflicts.txt."
- read -p "--- Press any key to continue --- "
- nano ${FOLDER_NAME}/conflicts.txt
- read -p "--Now please add the tweaks which should be removed if Gandalf is installed. Press any key to continue. --"
- nano ${FOLDER_NAME}/conflicts.txt
- echo "Starting to build current version..."
- # Add version to all_versions.txt
- printf "io.github.ethanrdoesmc.gandalf${FOLDER_NAME}\n" >> all_versions.txt
- ./build.sh ${FOLDER_NAME}
- build_all
+  read -p "Please enter shortname (eg. 'c'):" FOLDER_NAME
+  echo ${FOLDER_NAME}
+  mkdir ${FOLDER_NAME}
+  read -p "Please enter the supported firmware (format: =10.2 or <8.4):" FILE_FIRMWARE
+  printf "(${FILE_FIRMWARE})" > ${FOLDER_NAME}/firmware.txt
+  echo "Gandalf is usually named after a jailbreak: Gandalf for <jailbreakname>. So: "
+  read -p "Please enter the name of the jailbreak here:" FILE_NAME
+  printf "${FILE_NAME}" > ${FOLDER_NAME}/name.txt
+  read -p "Please enter the section now:" FILE_SECTION
+  printf "${FILE_SECTION}" > ${FOLDER_NAME}/section.txt
+  echo "Now there comes the most important step: you must now add all bundle identifiers to the file conflicts.txt."
+  read -p "--- Press any key to continue --- "
+  nano ${FOLDER_NAME}/conflicts.txt
+  read -p "--Now please add the tweaks which should be removed if Gandalf is installed. Press any key to continue. --"
+  nano ${FOLDER_NAME}/conflicts.txt
+  echo "Starting to build current version..."
+  # Add version to all_versions.txt
+  printf "io.github.ethanrdoesmc.gandalf${FOLDER_NAME}\n" >> all_versions.txt
+  ./build.sh ${FOLDER_NAME}
+  build_all
 ;;
+
 "clean")
   # Set and make tempdir. Thanks to https://unix.stackexchange.com/a/84980
  # should work on macOS and Linux. 
@@ -77,20 +80,23 @@ case $1 in
  sed -i 's/ *$//' all_versions.txt
 
  # Get shortname of versions in all_versions.txt by deleting io.github.ethanrdoesmc.gandalf; Sort conflicts.txt and replaces.txt of every known version. 
-echo "Sorting packages in conflicts.txt and replaces.txt"
+ echo "Sorting packages in conflicts.txt and replaces.txt"
  for VERSION in $(cat all_versions.txt | sed "s/^${PACKAGE_REGEX}//g"); do
- cat ${VERSION}/conflicts.txt | sort > ${TEMPDIRECTORY}/conflicts.txt
- cat ${TEMPDIRECTORY}/conflicts.txt > ${VERSION}/conflicts.txt
- cat ${VERSION}/replaces.txt | sort > ${TEMPDIRECTORY}/replaces.txt
- cat ${TEMPDIRECTORY}/replaces.txt > ${VERSION}/replaces.txt
+  cat ${VERSION}/conflicts.txt | sort > ${TEMPDIRECTORY}/conflicts.txt
+  cat ${TEMPDIRECTORY}/conflicts.txt > ${VERSION}/conflicts.txt
+  cat ${VERSION}/replaces.txt | sort > ${TEMPDIRECTORY}/replaces.txt
+  cat ${TEMPDIRECTORY}/replaces.txt > ${VERSION}/replaces.txt
 done
 echo "Cleaning up temporary folder..."
 rm -rf  ${TEMPDIRECTORY}
 ;;
+
 "setup-mac")
  echo "This will once setup your Mac, so that you can compile gandalf on it."
 ;;
+
 *)
- echo "${RED}ERROR:${NORMAL} Unknown parameter '$1'."
+  echo "${RED}ERROR:${NORMAL} Unknown parameter '$1'."
 ;;
+
 esac
