19616386f1796ba7603f785b66e073adbbe5cc3c
jonisc <jonisc@t-online.de>
2017-06-28 19:36:04 +0200

    Style

--- Gandalf/gdn-dev.sh
+++ Gandalf/gdn-dev.sh
@@ -6,15 +6,16 @@ BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
 RED=$(tput setaf 1)
 
-if [ "$1" = "" ];then
+# Check if script is launched without parameters and output error.
+if [ "$1" = "" ]; then
  echo "${BOLD}USAGE:${NORMAL} '$0 <option>'"
  exit 1
 fi
-
+# Regular expression of gandalf bundle identifier, we need 2 '\' because bash eats up the first one. 
 PACKAGE_REGEX="io\\.github\\.ethanrdoesmc\\.gandalf"
 # Functions
-# build_all: Build all gandalf versions at once
 
+# build_all: Build all gandalf versions at once
 function build_all () {
  echo "Building all gandalf versions..."
  # Get number of lines of the file all_versions.txt and save them in a variable (uncomment for debug)
@@ -24,7 +25,6 @@ function build_all () {
  # Get shortname of versions in all_versions.txt by deleting io.github.ethanrdoesmc.gandalf and compile gandalf as long as there is still a version --Comment not good, please edit--
 
   for VERSION in $(cat all_versions.txt | sed "s/^${PACKAGE_REGEX}//g"); do
-
    ./build.sh "${VERSION}"
 
    # Check if packaging was not successful; First save exitcode of build.sh in variable to make it possible to better work with it. 
@@ -40,9 +40,12 @@ function build_all () {
   done
 }
 
+# Main Switch: check with which parameter script is launched
 case $1 in
 "compile-for-repo")
+  # Compile all known gandalf versions
   build_all
+  # Move all debs to repo
   echo "Move packages to repo..."
   mv *.deb ../docs/debs/
   echo "Update repo..."
@@ -50,6 +53,7 @@ case $1 in
   rm ../docs/Packages
   rm ../docs/Packages.gz
   rm ../docs/Packages.bz2
+  # Create repo structure
   dpkg-scanpackages ../docs/debs > ../docs/Packages
   # Check if dpkg-scanpackages was not successful
   DPKG_SCANPKGERRORC=$(echo $?)
@@ -62,17 +66,20 @@ case $1 in
 ;;
 
 "compile-all")
+  # Compile all known gandalf versions
   build_all
 ;;
 
 "new")
+  # Create new gandalf version
+
   read -p "Please enter shortname (eg. 'c'):" FOLDER_NAME
   echo ${FOLDER_NAME}
   mkdir ${FOLDER_NAME}
   read -p "Please enter the supported firmware (format: =10.2 or <8.4):" FILE_FIRMWARE
   printf "(${FILE_FIRMWARE})" > ${FOLDER_NAME}/firmware.txt
   echo "Gandalf is usually named after a jailbreak: Gandalf for <jailbreakname>. So: "
-  read -p "Please enter the name of the jailbreak here:" FILE_NAME
+  read -p "Please enter the name of the jailbreak here:" FILE_NAM
   printf "${FILE_NAME}" > ${FOLDER_NAME}/name.txt
   read -p "Please enter the section now:" FILE_SECTION
   printf "${FILE_SECTION}" > ${FOLDER_NAME}/section.txt
@@ -89,27 +96,35 @@ case $1 in
 ;;
 
 "clean")
+  # Sort file contents alphabetically
   # Set and make tempdir. Thanks to https://unix.stackexchange.com/a/84980
   # should work on macOS and Linux.
 
   TEMPDIRECTORY=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir.XXXXXX')
+  # Sort versions in all_versions.txt alphabetically. 
+  echo 'Sorting all_versions.txt...'
+  cat all_versions.txt > ${TEMPDIRECTORY}/all_versions.txt
+  cat ${TEMPDIRECTORY}/all_versions.txt > all_versions.txt
+  # Clean all_versions.txt
   echo 'Removing all possible spaces at the end of the line to make the $ (anchor for end of line) work'
   sed -i 's/ *$//' all_versions.txt
-
   # Get shortname of versions in all_versions.txt by deleting io.github.ethanrdoesmc.gandalf; Sort conflicts.txt and replaces.txt of every known version. 
   echo "Sorting packages in conflicts.txt and replaces.txt"
   for VERSION in $(cat all_versions.txt | sed "s/^${PACKAGE_REGEX}//g"); do
+  # output file content, sort them, save them to a temporary file, and rewrite them back
    cat ${VERSION}/conflicts.txt | sort > ${TEMPDIRECTORY}/conflicts.txt
    cat ${TEMPDIRECTORY}/conflicts.txt > ${VERSION}/conflicts.txt
    cat ${VERSION}/replaces.txt | sort > ${TEMPDIRECTORY}/replaces.txt
    cat ${TEMPDIRECTORY}/replaces.txt > ${VERSION}/replaces.txt
 
   done
+  # Cleanup
   echo "Cleaning up temporary folder..."
   rm -rf  ${TEMPDIRECTORY}
 ;;
 
 "setup-mac")
+  # To be done...
   echo "This will once setup your Mac, so that you can compile gandalf on it."
 ;;
 
